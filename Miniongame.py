c6343e267318e4671931b63be8a06ad12a487dd1
Thomas <thomasjk10@gmail.com>
2018-06-28 23:04:10 +0530

    Update Miniongame.py

--- Miniongame.py
+++ Miniongame.py
@@ -9,18 +9,23 @@ ptss = ['Stuart', 0]
 t = 0
 len_s = len(s)
 
-print (len(s))
+#print (len(s))
 
 for i in s:
     t = t + 1
     if re.match(vowels,i):
         for n in range(len(s)-(t-1)):
-            #print (s[t-1:n+t])
+            strg = (s[t-1:n+t])
             ptsk[1] = ptsk[1] +1
     else:
         for n in range(len(s)-(t-1)):
             strg = (s[t-1:n+t])
             ptss[1] = ptss[1] +1
 
-print (max(ptss,ptsk)[0], end=" ")
-print (max(ptss,ptsk)[1])
+p = max(ptsk[1],ptss[1])
+if p in ptsk:
+    print (ptsk[0], end=" ")
+    print (ptsk[1])
+else:
+    print (ptss[0], end=" ")
+    print (ptss[1])
